6446fdf08f83e3dbe2a6a4a88492f4ddcda74366
sho16 <sho16@yandex.ru>
2018-04-05 13:22:00 +0300

    Create construct.java

---
+++ HomeWorks/Task5/construct.java
@@ -0,0 +1,23 @@
+
+
+class constr {
+   int a;
+   int b;
+  
+   
+   constr (int A, int B ) {
+	   a=A;
+	   b=B;
+	   
+      
+   }
+}
+public class construct{
+
+	   public static void main(String args[]) {
+		   constr t1 = new constr(10, 4);
+		System.out.println(t1.a / t1.b);
+	      
+	     
+	   }
+	}
