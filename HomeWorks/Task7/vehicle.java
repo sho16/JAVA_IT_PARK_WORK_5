56c1882133be2b51d4557dd044fa7af143720684
sho16 <sho16@yandex.ru>
2018-04-09 15:58:41 +0300

    Update vehicle.java

--- HomeWorks/Task7/vehicle.java
+++ HomeWorks/Task7/vehicle.java
@@ -1,12 +1,14 @@
 public class vehicle {
  
     public static void main(String[] args) {
-        Automobile Automobile = new Automobile( false, "diesel", 2, "black");
+        Automobile Automobile = new Automobile(false, "diesel",2, "red");
  
         System.out.println("Automobile is passanger?" + Automobile.isTruck());
         System.out.println("Automobile oil " + Automobile.getDiesel());
-        System.out.println("Automobile has  doors " + Automobile.getDoors() + " doors.");
+        System.out.println("Automobile has  " + Automobile.getDoors() + " doors.");
+        Automobile.setColor("Yelow");
         System.out.println("Automobile color is " + Automobile.getColor());
+        
     }
  
 }
